32fd8604502b597a1fcb3d4f741c9b9a34495b2c
falah <fajril.falah93@gmail.com>
2018-12-13 20:34:21 +0700

    create webpack reactjs

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve("dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                }
+            }
+        ]
+    },
+    mode: 'development'
+}
