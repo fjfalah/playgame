e450601e891d2bd96862acf6360138dd70d9fbc2
falah <fajril.falah93@gmail.com>
2018-12-15 16:28:11 +0700

    add content in pgBrandSection component

--- src/routes/about/components/pgBrandSection.js
+++ src/routes/about/components/pgBrandSection.js
@@ -1,27 +1,79 @@
 import React, { Component } from 'react'
 import { Row, Col, Text, Button } from '../../../components/common';
+import STYLES from '../../../constants/styles'
 
 export default class PgBrandSection extends Component {
     render() {
         return (
-            <Row>
-                <Col sm={12}>
-                    <Text h1 align="center">
-                        POWERING REAL ECONOMY IN THE
+            <div className="brandSection">
+                <Row>
+                    <Col sm={12}>
+                        <Text h1 align="center">
+                            POWERING REAL ECONOMY IN THE
                     </Text>
-                    <Text h1 bold align="center">
-                        GAMING WORLD
+                        <Text h1 weight="500" align="center">
+                            GAMING WORLD
                     </Text>
-                </Col>
-                <Col sm={12}>
-                    <Text h4 align="center" styles={{marginTop: '50',}}>
-                        <img src="../../../assets/images/img_bg_about.png" />
-                        JOIN OUR TELEGRAM
-                    </Text>
-                    <Button orange outline block>Yellow</Button>
-                    <Button blue outline>Blue</Button>
-                </Col>
-            </Row>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={12}>
+                        <Text h4 align="center" styles={{ marginTop: '50', }}>
+                            <img src="../../../assets/images/img_bg_about.png" />
+                            JOIN OUR TELEGRAM
+                        </Text>
+                    </Col>
+                </Row>
+                <Row justifyCenter>
+                    <Col md={4}>
+                        <Row>
+                            <Col md={6}>
+                                <Text weight="500" align="center">GROUP</Text>
+                                <Row>
+                                    <Col md={6}>
+                                        <Button blue outline block>
+                                            ENG
+                                        </Button>
+                                    </Col>
+                                    <Col md={6}>
+                                        <Button blue outline block>
+                                            IND
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col md={6}>
+                                <Text weight="500" align="center">CHANNEL</Text>
+                                <Row>
+                                    <Col md={12}>
+                                        <Button orange outline block>
+                                            PLAYGAMEICO
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row justifyCenter>
+                    <Col md={3}>
+                        <Text size="36px" align="right" weight="bold">
+                            ABOUT
+                        </Text>
+                        <Text size="36px" align="right" weight="bold" color={STYLES.color.orange}>
+                            PLAYGAME
+                        </Text>
+                    </Col>
+                    <Col md={6}>
+                        <Text size="18px" as="p">
+                            To simplify marketing and distribution, we are creating playgame.com. A direct-to-play gaming platform where players can play HTML5 games directly from their desktop, mobile browser or even embedded on their own application. No application downloads required. Publishing your game to playgame.com is not mandatory.
+                        </Text>
+                        <Text size="18px" as="p">
+                            We will also be launching a crypto-powered crowdfunding platform that we called the FunFund Initiative.
+                        </Text>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
